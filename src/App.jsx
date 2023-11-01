a280f9f11fb138565bafb65700cae1cbe2a33f2d
kareraolivier <kareraolivier@gmail.com>
2023-11-01 16:59:42 +0200

    fix: error fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import UserClicks from "./components/userclicks/UserClicks";
 import StateandProps from "./components/stateandprops/StateandProps";
 import MappingData from "./components/mappingdata/MappingData";
 import CompData from "./components/mapdatacomponent/CompData";
-import LoginForm from "./components/buildform/LoginForm";
+import LoginForm from "./components/buildform/Login";
 import RenderingData from "./components/renderingJSON/RenderingData";
 import FetchData from "./components/fetchdata/FetchData";
 import CustomComponent from "./components/customComponent/Customcomponent";
