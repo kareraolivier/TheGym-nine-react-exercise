57437fdbf8267583da2e345172a346e7fa9472d5
kareraolivier <kareraolivier@gmail.com>
2023-11-01 16:52:46 +0200

    fix: custom component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Hello from "./components/hello/Hello";
 import UserClicks from "./components/userclicks/UserClicks";
-import CustomComponent from "./components/customComponent/CustomComponent";
 import StateandProps from "./components/stateandprops/StateandProps";
 import MappingData from "./components/mappingdata/MappingData";
 import CompData from "./components/mapdatacomponent/CompData";
 import LoginForm from "./components/buildform/LoginForm";
 import RenderingData from "./components/renderingJSON/RenderingData";
 import FetchData from "./components/fetchdata/FetchData";
+import CustomComponent from "./components/customComponent/Customcomponent";
+
 function App() {
   return (
     <>
